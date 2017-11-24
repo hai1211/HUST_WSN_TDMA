14bcb037e72dbe6fe1bb23159a5691cf9d01e4dd
ducnguyen6431 <ducnguyen6431@gmail.com>
2017-11-24 01:51:31 +0100

    [ADD]  - CM5000 config file  - XM1000 config file [FEATURE]  - Rewrite run-general.sh for better automation

--- src/run-general.sh
+++ src/run-general.sh
@@ -11,6 +11,7 @@ usage() {
 port="null"
 addr="null"
 valid_addr=1
+
 debug=0
 is_telosb=0
 
@@ -21,6 +22,8 @@ parse_argument() {
 			break
 		fi
 		key="$1"
+
+
 		case $key in
 			-t|--telosb)
 			is_telosb=1
@@ -57,7 +60,6 @@ promt_to_continue() {
 	while read -ra sensor_type; do
 		echo "Device: ${sensor_type[2]}"
 	done < motelist.txt
-	rm -r motelist.txt
 	while true; do
 		read -p "Do you wish to install this program[Y/n]?" yn
 		case $yn in
@@ -84,6 +86,9 @@ promt_for_port() {
 	done
 }
 
+if [[ ! -d log ]]; then
+	mkdir log
+fi
 parse_argument $@
 addr_validation
 if [[ $valid_addr -le 0 ]]; then
@@ -103,7 +108,6 @@ while read -ra sensor_type; do
 		exit
 	fi
 done < motelist.txt
-rm -r motelist.txt
 if [[ $is_telosb -eq 1 ]]; then
 	echo "Using config file for CM5000"
 	cd CM5000
@@ -120,9 +124,11 @@ if [[ $is_telosb -eq 1 ]]; then
 else
 	sleep 7 && make xm1000 install.0x"$addr" bsl,/dev/ttyUSB"$port"
 fi
+clear_log
 if [[ $debug -eq 1 ]]; then
-	clear_log
 	echo ""	> log/log_"$port"_0x"$addr".txt
 	gnome-terminal -e "tail -f log/log_"$port"_0x"$addr".txt"
+
+
 	sleep 5 && java net.tinyos.tools.PrintfClient -comm serial@/dev/ttyUSB"$port":115200	> log/log_"$port"_0x"$addr".txt
 fi
